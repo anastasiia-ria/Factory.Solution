3f2899a378f71ab4f0cc62a899420c3e4d41cc72
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-03-18 13:46:13 -0700

    update Models

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -12,6 +12,7 @@ namespace Factory.Models
 
     public int MachineId { get; set; }
     public string Name { get; set; }
+    public string Status { get; set; }
     public virtual ICollection<EngineerMachine> JoinEntities { get; }
   }
 }
